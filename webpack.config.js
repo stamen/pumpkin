5a099e4482a92747d46c570afa73340fc46562ec
Damon <nomad.damon@gmail.com>
2022-10-26 10:38:41 -0700

    mv index

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, './dist'),
+      directory: path.join(__dirname, './'),
     },
     compress: true,
     port: 3000,
